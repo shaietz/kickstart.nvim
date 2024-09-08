5a3105be92d1619285666f2038f5b21506d3471f
Shai Etzion <etzshai1@gmail.com>
2024-09-08 20:19:48 +0300

    removed debug plugin, disabled wordwrap

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -8,19 +8,9 @@
 --  To update plugins you can run
 --    :Lazy update
 --
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-
-  require 'kickstart.plugins.debug',
 
   require 'kickstart.plugins.which-key',
 
@@ -40,20 +30,14 @@ require('lazy').setup({
 
   require 'kickstart.plugins.treesitter',
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
+
   require 'kickstart.plugins.lint',
+
   require 'kickstart.plugins.autopairs',
+
   require 'kickstart.plugins.neo-tree',
+
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
